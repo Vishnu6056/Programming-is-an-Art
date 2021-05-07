52907e90bf236955fd061d7a1a1d52c171d41ae8
Vishnu6056 <76236924+Vishnu6056@users.noreply.github.com>
2021-05-07 14:07:40 +0530

    Create productof2num.cpp
    This program displays the product of two number as an output and it was developed by Vishnu P

---
+++ productof2num.cpp
@@ -0,0 +1,10 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+float num1,num2,result;
+cout<<" Enter two numbers : ";
+cin>>num1>>num2;
+result=num1*num2;
+cout<<"Product = "<<result;
+}
